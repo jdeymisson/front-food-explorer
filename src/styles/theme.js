150cc6a868f357e6ead297e7f2531f47498b680b
Johnny Deymisson <jdeymisson@gmail.com>
2023-08-10 22:58:29 -0300

    :sparkles: feat: criando os estilos globais e theme, :recycle: removido arquivos padrão do vite, estilos, assets e tags de link do index.html

---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+export default {
+    COLORS: {
+        BACKGROUND_DARK_900: "#0D1D25", 
+        BACKGROUND_DARK_800: "#0D161B", 
+        BACKGROUND_DARK_700: "#001119", 
+        BACKGROUND_DARK_600: "#00111A", 
+        BACKGROUND_DARK_500: "#000C12", 
+        BACKGROUND_DARK_400: "#000A0F", 
+
+        WHITE_200: "#FFFAF1",
+        WHITE_100: "#FFF",
+
+        GRAY_500: "#4D585E", 
+        GRAY_400: "#76797B", 
+        GRAY_300: "#7C7C8A", 
+        GRAY_200: "#C4C4CC", 
+        GRAY_100: "#E1E1E6", 
+
+        RED: "#750310",   
+        LIGHT_GREEN: "#82F3FF",
+        LIGHT_BLUE:"#065E7C"
+        }
+}
